21a3494b5a61e1904e4c669cb144e907c36abf57
Luiza <lujoseds@gmail.com>
2022-06-23 18:42:32 -0300

    Atividade 1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,13 +8,13 @@ var emailOk = false
 
 function validarNome(){
     let txtNome = document.querySelector('#TxtNome')
-    if (nome.value.length > 2){
-        txtNome.innerHTML = 'válido'
+    if (nome.value.lenght >2){
+        txtNome.innerHTML = 'Valid'
         txtNome.style.color = 'green'
         nomeOk = true
     }
     else{
-        txtNome.innerHTML = 'Inválido'
+        txtNome.innerHTML = 'Invalid'
         txtNome.style.color = 'red'
         nomeOk = false
     }
@@ -23,12 +23,12 @@ function validarNome(){
 function validarEmail(){
     let txtEmail = document.querySelector('#TxtEmail')
     if (email.value.indexOf('@') > 0 && email.value.indexOf('.') > 0){
-    txtEmail.innerHTML = 'Válido'
+    txtEmail.innerHTML = 'Valid'
     txtEmail.style.color = 'green'
     emailOk = true
     }
     else{
-        txtEmail.innerHTML = ' Inválido'
+        txtEmail.innerHTML = ' Invalid'
         txtEmail.style.color = 'red'
         emailOk = false
     }
